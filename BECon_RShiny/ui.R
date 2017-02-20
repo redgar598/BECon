796dac6b915028fe7abc0216d9c4a3fa3e22fc83
Rachel Edgar <redgar598@gmail.com>
2017-02-20 11:19:59 -0800

    Update ui.R

--- BECon_RShiny/ui.R
+++ BECon_RShiny/ui.R
@@ -1,6 +1,10 @@
 shinyUI(fluidPage(
-  titlePanel(h1("BECon: A tool for interpreting DNA methylation findings from blood in the context of brain")),
+  titlePanel(h1("BECon: A tool for interpreting DNA methylation findings from blood in the context of brain"),
+             tags$head(tags$link(rel = "icon", type = "image/png", href = "overview_icon.png"),
+                       tags$title("BECon"))),
   
+  
+
   
   
   sidebarLayout(
@@ -9,13 +13,13 @@ shinyUI(fluidPage(
                      The aim of BECon (Blood-Brain Epigenetic Concordance) is to allow for improved interpretation of surrogate 
                      methylation results by looking at the relationship human blood and brain methylation."),
                   helpText(a("Click Here For More Information",href="Blood_brain_Shiny_Help.pdf", target="_blank")),
-                  
+
                   # CpG List Input
                   #textInput("cpgname", label = h3("Text input"), value = "cg00000109"),
                   selectizeInput('CpG_list', h4("CpGs To View"), choices=NULL, multiple = TRUE, 
                                  options = list(placeholder = 'Input CpG IDs')),
                   
-                  
+
                   # Gene Name input
                   #textInput("genename", label = h3("Gene Name"), value = "AADACL4"),                                
                   selectizeInput('gene_list', h4("Genes To View"), choices=NULL, multiple = TRUE, 
@@ -24,43 +28,43 @@ shinyUI(fluidPage(
                   
                   # maximum CpGs to view
                   numericInput("CpGnum", label = h4("Max CpG Number to Plot"), value = 12),
-                  
+                                
                   submitButton("Update View"),
                   img(src = "overview2.png", height = 320, width = 320),
                   width = 3),
-    
+                  
     
     
     mainPanel(
       # helpful bit of text
       textOutput("text2"),
-      
+
       
       uiOutput('sized_plot'),
-      
+    
       #co meth plot
       downloadLink('downloadPlot', 'Download Plots'),
       plotOutput("plot1", height=400, width = 1000),
       
       
-      #       #correlation plot
-      #       h4("Correlation Density Plot with Selected CpGs Indicated"),
-      #       plotOutput("plot2", height= 400,width=400),
-      #       #Varibility plot
-      #       h4("Varibility Density Plots with Selected CpGs Indicated"),
-      #       plotOutput("plot3", width=400),
+#       #correlation plot
+#       h4("Correlation Density Plot with Selected CpGs Indicated"),
+#       plotOutput("plot2", height= 400,width=400),
+#       #Varibility plot
+#       h4("Varibility Density Plots with Selected CpGs Indicated"),
+#       plotOutput("plot3", width=400),
       
       # summary table and plot
-      h4("Summary of the Correlations and Varibility of Selected CpGs"),
+      h4("Summary of the Correlations and Variability of Selected CpGs"),
       "Download as a", 
       downloadLink('downloadPlotTbl', 'colorful table'), "or a",
       downloadLink('downloadData', 'csv file'),
       plotOutput("plotsum",width = 1200))
+
+      #tableOutput("view"))
+      # Summary plot
     
-    #tableOutput("view"))
-    # Summary plot
+
     
-    
-    
-    
-    )))
+
+)))
