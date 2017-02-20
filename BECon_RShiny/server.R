e7e7bfb766e04be9d5df6b64cb975b2c42837015
Rachel Edgar <redgar598@gmail.com>
2017-02-20 11:19:17 -0800

    Update server.R

--- BECon_RShiny/server.R
+++ BECon_RShiny/server.R
@@ -1,56 +1,84 @@
 library(ggplot2)
 library(reshape)
 
-#setwd("~/BLBR/BLBR_app/data")
 
-#setwd("/srv/shiny-server/BLBR_app/data") 
 options(shiny.maxRequestSize=30*1024^2)
 
-#load("./data/BLBR_app_Objects_old.RData")
-load("./data/BLBR_app_Objects.RData")
+load("./data/BLBR_app_Objects_beta.RData")
+#load("~/BECon/data/BLBR_app_Objects_beta.RData")
 
-
-levels(meta$TissueType)<-c("Brodmann Area 10","Brodmann Area 20",
-                           "Brodmann Area 7","Whole Blood")
-
+ 
 # ## density plots stuff
 # correlations_BLBR_densityplt<-correlations_BLBR
 # correlations_BLBR_densityplt$mean<-rowMeans(correlations_BLBR_densityplt[,2:4])
 # 
 # var_tissues_densityplt<-var_tissues[,1:3]
 # var_tissues_densityplt<-melt(var_tissues_densityplt, id="CpG")
-# colnames(var_tissues_densityplt)<-c("CpG","Tissue","Varibility")
+# colnames(var_tissues_densityplt)<-c("CpG","Tissue","Variability")
 # levels(var_tissues_densityplt$Tissue)<-c("Blood","All Brain")
 
 #### input summary
 CpG_list<-function(gene_interest, CpGs){
-  Genes<-Gene_CpG_Relations_minimal[which(Gene_CpG_Relations_minimal$gene%in%gene_interest | Gene_CpG_Relations_minimal$Probe_ID%in%CpGs),]
+  if(is.character(gene_interest)){ 
+    Genes<-lapply(1:length(gene_interest), function(x){
+      Genes<-Gene_CpG_Relations_minimal[c(grep(paste(", ",gene_interest[x],sep=""), Gene_CpG_Relations_minimal$gene),
+                                          grep(paste(gene_interest[x],",",sep=""), Gene_CpG_Relations_minimal$gene),
+                                          which(Gene_CpG_Relations_minimal$gene==gene_interest[x]),
+                                          which(Gene_CpG_Relations_minimal$Probe_ID%in%CpGs)),]
+      list_gene<-as.list(Genes$gene)
+      list_gene<-sapply(list_gene, function(x) strsplit(x, ", "))
+      inlist<-unlist(sapply(1:length(list_gene), function(genes) if(any(list_gene[[genes]]==gene_interest[x])){genes}else{}))
+      Genes<-Genes[inlist,]
+      Genes})
+    Genes<-do.call(rbind, Genes)
+    
+  }else{
+    Genes<-Gene_CpG_Relations_minimal[which(Gene_CpG_Relations_minimal$Probe_ID%in%CpGs),]}
   
   Genes_onarray<-unique(Genes$gene)
+  Genes_onarray<-unique(unlist(strsplit(Genes_onarray, ",| ,|, ")))
   CpGs<-unique(c(as.character(Genes$Probe_ID), CpGs)) 
   withcorrelation<-nrow(correlations_BLBR[which(correlations_BLBR$CpG%in%CpGs),])
   paste(length(CpGs), "CpGs associated with ", length(Genes_onarray), "genes to look at (",
         withcorrelation, "CpGs have correlation data between blood and brain).", sep=" ")
 }
 
+
 ### make CpG list for other inputs
 CpG_list_forplottable<-function(gene_interest, CpGs){
-  Genes<-Gene_CpG_Relations_update[which(Gene_CpG_Relations_update$gene%in%gene_interest | Gene_CpG_Relations_update$Probe_ID%in%CpGs),]
   
-  Genes_onarray<-unique(Genes$gene)
+  if(is.character(gene_interest)){ 
+    Genes<-lapply(1:length(gene_interest), function(x){
+      Genes<-Gene_CpG_Relations_minimal[c(grep(paste(", ",gene_interest[x],sep=""), Gene_CpG_Relations_minimal$gene),
+                                          grep(paste(gene_interest[x],",",sep=""), Gene_CpG_Relations_minimal$gene),
+                                          which(Gene_CpG_Relations_minimal$gene==gene_interest[x]),
+                                          which(Gene_CpG_Relations_minimal$Probe_ID%in%CpGs)),]
+      list_gene<-as.list(Genes$gene)
+      list_gene<-sapply(list_gene, function(x) strsplit(x, ", "))
+      inlist<-unlist(sapply(1:length(list_gene), function(genes) if(any(list_gene[[genes]]==gene_interest[x])){genes}else{}))
+      Genes<-Genes[inlist,]
+      Genes})
+    Genes<-do.call(rbind, Genes)
+    
+  }else{
+    Genes<-Gene_CpG_Relations_minimal[which(Gene_CpG_Relations_minimal$Probe_ID%in%CpGs),]}
   CpGs<-unique(c(as.character(Genes$Probe_ID), CpGs)) 
   CpGs
 }
 
+
 ################## define drop downs
 CpG_names<-as.character(rownames(combat_BLBR_Beta_adjusted))
 #gene_names<-as.character(unique(Gene_CpG_Relations_minimal$gene))
-gene_names<-as.character(unique(Gene_CpG_Relations_update$gene))
+gene_names<-Gene_CpG_Relations_update_genes
 
 
 ############################## Comethylation Plot
 Comethylation_Plot<-function(correlations_BLBR, Betas, CpG_Hit_List, MaxCpGNum){
   hits_BLBR<-correlations_BLBR[which(correlations_BLBR$CpG%in%CpG_Hit_List),]
+  hits_BLBR<-merge(hits_BLBR, Gene_CpG_Relations_minimal[,1:3],  by.x="CpG",by.y="Probe_ID")
+  hits_BLBR<-hits_BLBR[with(hits_BLBR, (order(Chromosome_37, Coordinate_37))), ]
+  
   if(nrow(hits_BLBR)>MaxCpGNum){hits_BLBR<-hits_BLBR[1:MaxCpGNum,]}else{}
   
   nrow(hits_BLBR)
@@ -67,15 +95,23 @@ Comethylation_Plot<-function(correlations_BLBR, Betas, CpG_Hit_List, MaxCpGNum){
   BLBR_Beta<-melt(BLBR_Beta)
   BLBR_Beta<-merge(BLBR_Beta, meta, by.x="variable", by.y="X")
   BLBR_Beta<-merge(BLBR_Beta, Gene_CpG, by.x="CpG", by.y="Probe_ID")
+  BLBR_Beta<-BLBR_Beta[with(BLBR_Beta, (order(Chromosome_37, Coordinate_37))),]
+  BLBR_Beta$Gene_CpG<-as.factor(BLBR_Beta$Gene_CpG)
   
+  BLBR_Beta_order<-BLBR_Beta[,c("Gene_CpG","Coordinate_37","Chromosome_37")]
+  BLBR_Beta_order<-BLBR_Beta_order[!duplicated(BLBR_Beta_order), ]
+  BLBR_Beta_order<-BLBR_Beta_order$Gene_CpG[with(BLBR_Beta_order, (order(Chromosome_37, Coordinate_37)))]
+  
+  BLBR_Beta$Gene_CpG<-factor(BLBR_Beta$Gene_CpG, levels=as.character(BLBR_Beta_order))
+  levels(BLBR_Beta$TissueType)<-c("Brodmann area 10", "Brodmann area 20", "Brodmann area 7", "Blood")
   
   ggplot()+geom_line(aes(SubNumber, value,group=TissueType,color=TissueType),#,alpha=line
                      BLBR_Beta, size=1.5)+
     theme_bw()+facet_wrap(~Gene_CpG)+
     scale_color_manual(values=c("#fb6a4a","#ef3b2c","#cb181d","cornflowerblue"))+
     ylim(0,1)+xlab("Subject Number")+ylab("Beta Value")+
-    theme(text = element_text(size=15))}
-
+    theme(text = element_text(size=15))
+}
 
 ############################### SUmmary Table
 summTable<-function(CpGs){
@@ -90,7 +126,7 @@ summTable<-function(CpGs){
   varibility<-var_tissues[which(var_tissues$CpG%in%CpGs),]
   CpG_gene_correlations_varibility<-merge(CpG_gene_correlations, var_tissues, by.x="Probe_ID", by.y="CpG")
   CpG_gene_correlations_varibility<-CpG_gene_correlations_varibility[order(CpG_gene_correlations_varibility$Probe_ID),]
-  
+
   CpG_gene_correlations_varibility$Coordinate_37<-as.numeric(as.character(CpG_gene_correlations_varibility$Coordinate_37))
   CpG_gene_correlations_varibility<-CpG_gene_correlations_varibility[with(CpG_gene_correlations_varibility, order(gene, Coordinate_37)), ]
   
@@ -130,7 +166,7 @@ color_possibilities<-c("Genomic Info", levels(var_tissues$Var_blood_color), leve
                        levels(correlations_BLBR$Blood_CellComp_Percentile), levels(correlations_BLBR$Brain_CellComp_Percentile))
 
 names(myColors) <- color_possibilities
-fillscale <- scale_fill_manual(name = "Correlation or Cell Composition Percentile\n or Varibility Status",values = myColors, drop = FALSE)
+fillscale <- scale_fill_manual(name = "Correlation or Cell Composition Percentile\n or Variability Status",values = myColors, drop = FALSE)
 
 
 
@@ -146,7 +182,7 @@ Colorful_table_plot<-function(CpGs){
   
   color_table_plot_values<-rbind(color_table_plot_cor_values, color_table_plot_var_values)
   color_table_plot_values$Data<-as.factor(color_table_plot_values$variable)
-  levels(color_table_plot_values$Data)<-c("correlations","correlations","correlations","Cell_composition","Cell_composition","varibility","varibility","varibility","varibility")
+  levels(color_table_plot_values$Data)<-c("correlations","correlations","correlations","Cell_composition","Cell_composition","variability","variability","variability","variability")
   color_table_plot_color<-rbind(color_table_plot_cor_color, color_table_plot_var_color)
   color_table_plot_values$color<-color_table_plot_color$value
   color_table_plot_values<-color_table_plot_values[,c(1,4,2,3,5)]
@@ -168,26 +204,40 @@ Colorful_table_plot<-function(CpGs){
   ## final data
   cor_var_for_plot<-rbind(color_table_plot_values, CpG_gene_melt)
   levels(cor_var_for_plot$variable)<-c("BA7","BA10","BA20","Blood","Brain","Blood","BA7","BA10","BA20","")
-  levels(cor_var_for_plot$Data)<-c("Correlation","Cell Composition","Varibility","Chr","Coor","Gene(s)","Gene \nRegion(s)")
+  levels(cor_var_for_plot$Data)<-c("Correlation","Cell \nComposition","Variability","Chr","Coor","Gene(s)","Gene \nRegion(s)")
   
   cor_var_for_plot$Data<-factor(cor_var_for_plot$Data, levels=c("Chr","Coor","Gene(s)","Gene \nRegion(s)",
-                                                                "Varibility","Correlation","Cell Composition"))
+                                                                "Variability","Correlation","Cell \nComposition"))
   
   cor_var_for_plot$CpG<-as.factor(cor_var_for_plot$CpG)
   #CpG order
   CpG_order<-Gene_CpG_Relations_minimal[which(Gene_CpG_Relations_minimal$Probe_ID%in%CpGs),]
-  CpG_order<-CpG_order[with(CpG_order, order(gene, Coordinate_37)), ]
+  CpG_order<-CpG_order[with(CpG_order, rev(order(Chromosome_37, Coordinate_37))), ]
   
   cor_var_for_plot$CpG<-factor(cor_var_for_plot$CpG, 
                                levels=CpG_order$Probe_ID)
   
-  ggplot(cor_var_for_plot, aes(variable, CpG, fill = color)) +
+# #width
+  cor_var_for_plot$variable<-as.character(cor_var_for_plot$variable)
+  cor_var_for_plot$variable<-sapply(1:nrow(cor_var_for_plot), function(x) {
+  if(cor_var_for_plot$Data[x]=="Chr"){"  "}else{
+    if(cor_var_for_plot$Data[x]=="Coor"){"   "}else{
+      cor_var_for_plot$variable[x]}}
+    })
+  
+cor_var_for_plot$variable<-as.factor(cor_var_for_plot$variable)
+cor_var_for_plot$w<-cor_var_for_plot$variable
+levels(cor_var_for_plot$w)<-c(4,0.6,2.8,1,1,1,1,1)
+cor_var_for_plot$w<-as.numeric(as.character(cor_var_for_plot$w))
+  
+  ggplot(cor_var_for_plot, aes(variable, CpG, fill = color, width=w)) + #
     geom_tile(color = "black",size=0.5) +
-    theme_gray(8)+facet_grid(.~Data, scales="free_x",space="free_x")+
+    theme_gray(8)+ scale_x_discrete(position = "top")+
+    facet_grid(.~Data, scales="free_x",space="free_x")+
     geom_text(aes(label=value),color="black")+ #aes(label = round(value,2)), 
     fillscale+xlab("")+ylab("")+
     theme_minimal()+ guides(fill=guide_legend(ncol=2))+
-    theme(axis.text.x = element_text(size =12, color="black",margin=margin(-15,0,0,0), face="bold"),
+    theme(axis.text.x = element_text(size =10, color="black",margin=margin(0,0,0,0), face="bold"),
           axis.text.y = element_text(size =12, color="black"),
           axis.ticks = element_blank(),
           axis.title = element_text(size =15),
@@ -195,6 +245,7 @@ Colorful_table_plot<-function(CpGs){
           legend.text = element_text(size =10),
           legend.title = element_text(size =12),
           strip.text.x = element_text(size = 12, face="bold"))
+    
 }
 
 
@@ -235,36 +286,36 @@ Colorful_table_plot<-function(CpGs){
 shinyServer(function(input, output, session) {
   
   ## trying to get the CpG list drop down menu 
-  
+
   updateSelectizeInput(session, 'CpG_list', choices = CpG_names, server = TRUE)
   
   ## trying to get the gene list drop down menu 
-  
+
   updateSelectizeInput(session, 'gene_list', choices = gene_names, server = TRUE) 
   
   
   # helpful text output
   output$text2 <- renderText({paste("You have selected", CpG_list(input$gene_list, input$CpG_list)) })
-  #   
-  #   ## File input
-  #         output$contents <- renderTable({
-  #           # input$file1 will be NULL initially. After the user selects
-  #           # and uploads a file, it will be a data frame with 'name',
-  #           # 'size', 'type', and 'datapath' columns. The 'datapath'
-  #           # column will contain the local filenames where the data can
-  #           # be found.
-  #           
-  #           inFile <- input$file1
-  #           
-  #           if (is.null(inFile))
-  #             return(NULL)
-  #           
-  #           read.csv(inFile$datapath, header = input$header,
-  #                    sep = input$sep, quote = input$quote)
-  #         })
+#   
+#   ## File input
+#         output$contents <- renderTable({
+#           # input$file1 will be NULL initially. After the user selects
+#           # and uploads a file, it will be a data frame with 'name',
+#           # 'size', 'type', and 'datapath' columns. The 'datapath'
+#           # column will contain the local filenames where the data can
+#           # be found.
+#           
+#           inFile <- input$file1
+#           
+#           if (is.null(inFile))
+#             return(NULL)
+#           
+#           read.csv(inFile$datapath, header = input$header,
+#                    sep = input$sep, quote = input$quote)
+#         })
   
   
-  
+
   ####### COMETHYLATION PLOTS
   output$plot1<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
     plt<-Comethylation_Plot(correlations_BLBR,combat_BLBR_Beta_adjusted, c("cg00308130","cg00201133"), input$CpGnum)
@@ -273,21 +324,21 @@ shinyServer(function(input, output, session) {
       print(plt)}
   })
   
-  #   plotHeight_cometh = reactive({
-  #     if (input$CpGnum < length(CpG_list_forplottable(input$gene_list,input$CpG_list))){
-  #       length(CpG_list_forplottable(input$gene_list,input$CpG_list))*100
-  #     } else {
-  #       if (input$CpGnum < 10){
-  #         return(400)}
-  #       else {
-  #         input$CpGnum * 100}}
-  #   })
-  #   
-  #   
-  #   output$sized_plot <- renderUI({
-  #     plotOutput("plot1",  height = input$CpGnum)
-  #   })
-  #   
+#   plotHeight_cometh = reactive({
+#     if (input$CpGnum < length(CpG_list_forplottable(input$gene_list,input$CpG_list))){
+#       length(CpG_list_forplottable(input$gene_list,input$CpG_list))*100
+#     } else {
+#       if (input$CpGnum < 10){
+#         return(400)}
+#       else {
+#         input$CpGnum * 100}}
+#   })
+#   
+#   
+#   output$sized_plot <- renderUI({
+#     plotOutput("plot1",  height = input$CpGnum)
+#   })
+#   
   
   
   # Download plots
@@ -304,12 +355,12 @@ shinyServer(function(input, output, session) {
       plt_dwn()
       dev.off()
     })
+
+  
+
   
   
-  
-  
-  
-  ############## Summary Table Render
+############## Summary Table Render
   output$view <- renderTable({
     if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
       summTable(c("cg00308130","cg00201133"))
@@ -323,13 +374,13 @@ shinyServer(function(input, output, session) {
     summTable(CpG_list_forplottable(input$gene_list,input$CpG_list))}})
   
   output$downloadData <- downloadHandler(
-    filename = function() {
-      paste('data-', Sys.Date(), '.csv', sep='')
-    },
-    content = function(con) {
-      write.csv(tab(), con)
-    })
-  
+     filename = function() {
+       paste('data-', Sys.Date(), '.csv', sep='')
+     },
+     content = function(con) {
+       write.csv(tab(), con)
+     })
+ 
   
   #TABLE PLOT
   ## define height of plots depenedent on the CpGs used
@@ -337,7 +388,7 @@ shinyServer(function(input, output, session) {
     if (length(CpG_list_forplottable(input$gene_list,input$CpG_list)) < 10) 
       return(300)
     length(CpG_list_forplottable(input$gene_list,input$CpG_list)) * 40
-  })
+           })
   
   output$plotsum<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
     plt<-Colorful_table_plot(c("cg00308130","cg00201133"))
@@ -346,7 +397,7 @@ shinyServer(function(input, output, session) {
       print(plt)}
   }, height=plotHeight,units="px")
   
-  
+
   # Download plots
   plt_tbl_dwn<-reactive({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
     plt<-Colorful_table_plot(c("cg00308130","cg00201133"))
@@ -363,44 +414,44 @@ shinyServer(function(input, output, session) {
       plt_tbl_dwn()
       dev.off()
     })
+    
+
   
   
+#   ######### Correlation density plot
+#   output$plot2<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
+#     plt<-cor_density_plot(c("cg00308130","cg00201133"))
+#     print(plt)}else{
+#       plt<-cor_density_plot(CpG_list_forplottable(input$gene_list,input$CpG_list))
+#       print(plt)}
+#   })
+#   
+#   ########## Variability density plot
+#   output$plot3<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
+#     plt<-var_density_plot(c("cg00308130","cg00201133"))
+#     print(plt)}else{
+#       plt<-var_density_plot(CpG_list_forplottable(input$gene_list,input$CpG_list))
+#       print(plt)}
+#   })
+#   
   
-  
-  #   ######### Correlation density plot
-  #   output$plot2<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
-  #     plt<-cor_density_plot(c("cg00308130","cg00201133"))
-  #     print(plt)}else{
-  #       plt<-cor_density_plot(CpG_list_forplottable(input$gene_list,input$CpG_list))
-  #       print(plt)}
-  #   })
-  #   
-  #   ########## Varibility density plot
-  #   output$plot3<-renderPlot({if(length(CpG_list_forplottable(input$gene_list,input$CpG_list))==0){
-  #     plt<-var_density_plot(c("cg00308130","cg00201133"))
-  #     print(plt)}else{
-  #       plt<-var_density_plot(CpG_list_forplottable(input$gene_list,input$CpG_list))
-  #       print(plt)}
-  #   })
-  #   
-  
-  
+ 
   
   ##output$plot1<-renderPlot(plt<-Comethylation_Plot(correlations_BLBR,combat_BLBR_Beta_adjusted, CpG_list_forplottable(input$gene_list,input$CpG_list), input$CpGnum)
-  #print(plt)})
+    #print(plt)})
   #output$view <- renderTable({
   #summTable(CpG_list_forplottable(input$gene_list,input$CpG_list))}, include.rownames=FALSE)
   
   
-  # output$view <- renderTable({
-  #summTable(CpG_list_forplottable(input$genename,input$cpgname))}, include.rownames=FALSE)
-  
+    # output$view <- renderTable({
+    #summTable(CpG_list_forplottable(input$genename,input$cpgname))}, include.rownames=FALSE)
+
   #output$plot1<-renderPlot({plt<-Comethylation_Plot(correlations_BLBR,combat_BLBR_Beta_adjusted, correlations_BLBR$CpG[1:input$CpGnum])
-  #print(plt)})
+                            #print(plt)})
   
   # Show the first "n" observations
   #output$view <- renderTable({
-  #summTable(correlations_BLBR$CpG[1:input$CpGnum])}, include.rownames=FALSE)
+    #summTable(correlations_BLBR$CpG[1:input$CpGnum])}, include.rownames=FALSE)
   
   
 })
